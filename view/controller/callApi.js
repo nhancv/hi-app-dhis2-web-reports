9e420fc334445ac75b5204764e833b038a56f297
Nhan Cao <caovannhan2002@gmail.com>
2016-02-09 15:40:23 +0530

    call api example

---
+++ view/controller/callApi.js
@@ -0,0 +1,21 @@
+/**
+ * Created by nhancao on 2/9/16.
+ */
+
+function callAjax(apiUrl, username, password, callback){
+    var form_data = {
+        url: apiUrl,
+        u: username,
+        p: password
+    };
+    $.ajax({
+        type: 'POST',
+        url: "http://localhost/Phplibrary/mbasic_au.php",
+        crossDomain: true,
+        dataType: 'json',
+        data: form_data,
+        cache: false
+    })
+        .done(callback);
+}
+
